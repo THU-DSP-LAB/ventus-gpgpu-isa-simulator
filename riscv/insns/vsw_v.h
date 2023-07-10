e6cf6c69c69d08e479a04af1b465102eb16fddba
yangkex <962284474@qq.com>
2023-07-10 15:32:52 +0800

    change vlw rs1 into vs1

--- riscv/insns/vsw_v.h
+++ riscv/insns/vsw_v.h
@@ -1,1 +1,3 @@
+baseAddr = index[i] + insn.v_simm11();
+baseBias = P.get_csr(CSR_PDS) + (baseAddr & ~3) * P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) + (baseAddr & 3);
 VI_GPU_ST_INDEX(e32,true,{MMU.store_uint32(baseBias + ((baseTid + vreg_inx)<<2),index[i]);});
