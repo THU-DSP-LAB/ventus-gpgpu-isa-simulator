e386350b59d3488ccaa74c5a246dd90848f001a4
Yangzx <yang-zx21@mails.tsinghua.edu.cn>
2023-08-30 12:17:25 +0800

    [fix] fixed bug that instruction vsw.v can't access right address

--- riscv/insns/vsw_v.h
+++ riscv/insns/vsw_v.h
@@ -4,8 +4,8 @@ VI_GPU_ST_INDEX(e32,true,(
     // reg_t baseBias = P.get_csr(CSR_PDS) + (baseAddr & ~3) * P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) + (baseAddr & 3);
     reg_t baseBias = P.get_csr(CSR_PDS) + (P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) * ((baseAddr >> 2)<<2));
 
-    std::cout << "insn:"<<insn.bits()<<";index="<<index[i]<<";imm="<<insn.v_s_simm11() \
-    <<";baseaddr="<<baseAddr<<";basebias="<<baseBias<<";vreg_inx="<<vreg_inx<<";store_addr="<<(baseBias+((baseTid + vreg_inx)<<2))<<std::endl;
+    // std::cout << "insn:"<<insn.bits()<<";index="<<index[i]<<";imm="<<insn.v_s_simm11() \
+    // <<";baseaddr="<<baseAddr<<";basebias="<<baseBias<<";vreg_inx="<<vreg_inx<<";store_addr="<<(baseBias+((baseTid + vreg_inx)<<2))<<std::endl;
 
     MMU.store_uint32(baseBias+((baseTid + vreg_inx)<<2),P.VU.elt<uint32_t>(2,vs2, vreg_inx));
     
