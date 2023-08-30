f6082d096ccd712d17196ce1f9362dfa2d6412e0
Yangzx <yang-zx21@mails.tsinghua.edu.cn>
2023-08-30 16:41:48 +0800

    modified defination of instruction vlw.v

--- riscv/insns/vlw_v.h
+++ riscv/insns/vlw_v.h
@@ -3,7 +3,7 @@ VI_GPU_LD_INDEX(e32,true,({
     // reg_t baseBias = P.get_csr(CSR_PDS) + (baseAddr & ~3) * P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) + (baseAddr & 3);
     // MMU.load_int32(baseBias+((baseTid + vreg_inx)<<2));}
 
-    reg_t baseBias = P.get_csr(CSR_PDS) + (P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) * (baseAddr >> 2));
-    MMU.load_int32(baseBias + vreg_inx);}
+    reg_t baseBias = P.get_csr(CSR_PDS) + (P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) * ((baseAddr >> 2)<<2));
+    MMU.load_int32(baseBias+((baseTid + vreg_inx)<<2));}
 ));
 
