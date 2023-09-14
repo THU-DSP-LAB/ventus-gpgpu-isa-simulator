141bbed72b09e39017d2d65d63dbc5e18568f545
Yangzx <yang-zx21@mails.tsinghua.edu.cn>
2023-09-14 16:30:39 +0800

    [bug] inst join should compare pc with r_pc of top simt_stack entry rather than csr_rpc

--- riscv/insns/join.h
+++ riscv/insns/join.h
@@ -1,4 +1,4 @@
 if(!(P.gpgpu_unit.simt_stack.stack_empty()))
-    if( pc ==  P.get_csr(CSR_RPC)){
+    if( pc ==  P.gpgpu_unit.simt_stack.top().r_pc){
         SET_PC(P.gpgpu_unit.simt_stack.top().else_pc);
         P.gpgpu_unit.simt_stack.pop_join();}
