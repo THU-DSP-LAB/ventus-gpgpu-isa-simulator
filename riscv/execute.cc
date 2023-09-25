c86520100f085d20fc27d4f65c7b378cfe12f552
Yangzx <yang-zx21@mails.tsinghua.edu.cn>
2023-09-25 11:36:45 +0800

    add space between every 4 bytes register data in log file

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -41,11 +41,11 @@ static void commit_log_print_value(FILE *log_file, int width, const void *data)
     default:
       // max lengh of vector
       if (((width - 1) & width) == 0) {
-        const uint64_t *arr = (const uint64_t *)data;
+        const uint32_t *arr = (const uint32_t *)data;
 
         fprintf(log_file, "0x");
-        for (int idx = width / 64 - 1; idx >= 0; --idx) {
-          fprintf(log_file, "%016" PRIx64, arr[idx]);
+        for (int idx = width / 32 - 1; idx >= 0; --idx) {
+          fprintf(log_file, "%08" PRIx32" ", arr[idx]);
         }
       } else {
         abort();
