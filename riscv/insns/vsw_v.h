ea3daac594a0fd436bc895fbc6834aa08ac27593
Mingyuan <cnwhmmy@outlook.com>
2024-12-28 15:11:06 +0800

    fix print vlw/vsw inst info error in disasm. reorganize code. did not change instruction's behavior.

--- riscv/insns/vsw_v.h
+++ riscv/insns/vsw_v.h
@@ -1,8 +1,5 @@
-VI_GPU_ST_INDEX(e32,true,(
-    {
-    reg_t baseAddr = index[i] + insn.v_s_simm11();
-    reg_t baseBias = P.get_csr(CSR_PDS) + (P.get_csr(CSR_NUMW) * P.get_csr(CSR_NUMT) * ((baseAddr >> 2)<<2));
-    MMU.store_uint32(baseBias+((baseTid + vreg_inx)<<2),P.VU.elt<uint32_t>(2,vs2, vreg_inx));
+VI_GPU_ST_INDEX(e32,true,({
+    MMU.store_uint32(baseBias+((baseTid + vreg_inx)<<2 + (baseAddr & 3)),P.VU.elt<uint32_t>(2,vs2, vreg_inx));
     }
     ));
 
